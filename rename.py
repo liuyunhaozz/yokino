8b857fe4f4843d32836e61677c43dbe8d8e91ccf
liuyunhaozz <2210532036@qq.com>
2021-06-26 23:58:32 +0800

    Add 334 imgs of yokino and a python file for rename

---
+++ rename.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+# You can change the name of img by this file to meet your requests
+import os, sys
+
+# cdir = os.getcwd()
+# print(cdir)
+dir = os.listdir()
+dir.remove('rename.py')
+# print(dir)
+for index, name in enumerate(dir):
+    os.rename(name, 'yokino_' + str(index + 1) + '.jpg')
+print('Rename Success!')
